1dd8b8711ae43a577cc3d8866f6970fc8d2858f9
Furkan <89530770+Furkan3498@users.noreply.github.com>
2024-07-02 11:07:31 +0300

    Controller

--- src/main/java/com/furkanceylan/secondhand/controller/UserController.java
+++ src/main/java/com/furkanceylan/secondhand/controller/UserController.java
@@ -6,6 +6,7 @@ import com.furkanceylan.secondhand.dto.UpdateUserRequest;
 import com.furkanceylan.secondhand.dto.UserDto;
 import com.furkanceylan.secondhand.model.User;
 import com.furkanceylan.secondhand.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,8 @@ import java.util.List;
 @RequestMapping("/v1/user")
 public class UserController {
 
+
+    //@Autowired
     private final UserService userService;
 
 
